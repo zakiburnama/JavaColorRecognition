30122eb65052cbf71e7e2cab1d2d18324db173b6
Zaki Burnama <zaki.burnama1017@gmail.com>
2023-09-20 22:46:34 +0700

    adjust spalsh screen

--- app/src/main/java/com/example/colorrecognitionjavaalpha/SplashScreenActivity.java
+++ app/src/main/java/com/example/colorrecognitionjavaalpha/SplashScreenActivity.java
@@ -6,6 +6,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
@@ -13,12 +15,15 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+//                Intent intent = new Intent(SplashScreenActivity.this, IntroActivity.class);
                 startActivity(intent);
                 finish();
             }
