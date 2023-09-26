1b1c14c9a73e10cc1a87e6622ac171f97ea9387b
Zaki Burnama <zaki.burnama1017@gmail.com>
2023-09-27 03:25:21 +0700

    make introduction level menu

---
+++ app/src/main/java/com/example/colorrecognitionjavaalpha/IntroductionFragment.java
@@ -0,0 +1,86 @@
+package com.example.colorrecognitionjavaalpha;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link IntroductionFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class IntroductionFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public IntroductionFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment IntroductionFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static IntroductionFragment newInstance(String param1, String param2) {
+        IntroductionFragment fragment = new IntroductionFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_introduction, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MaterialButton btnIntro1 = view.findViewById(R.id.button_introduction_1);
+        MaterialButton btnIntro2 = view.findViewById(R.id.button_introduction_2);
+        MaterialButton btnIntro3 = view.findViewById(R.id.button_introduction_3);
+
+        btnIntro1.setOnClickListener(this::onClick);
+        btnIntro2.setOnClickListener(this::onClick);
+        btnIntro3.setOnClickListener(this::onClick);
+    }
+
+    public void onClick(View view) {
+        Toast.makeText(getContext(),"Not Avaiable Yet", Toast.LENGTH_SHORT).show();
+    }
+}
