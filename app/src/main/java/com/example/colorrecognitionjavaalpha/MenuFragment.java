aab40979ca30941c7d2b088b65bf9ece71620761
Zaki Burnama <zaki.burnama1017@gmail.com>
2023-09-26 19:06:31 +0700

    connect to popup window

--- app/src/main/java/com/example/colorrecognitionjavaalpha/MenuFragment.java
+++ app/src/main/java/com/example/colorrecognitionjavaalpha/MenuFragment.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 public class MenuFragment extends Fragment {
@@ -37,13 +43,17 @@ public class MenuFragment extends Fragment {
         btnBeginner.setOnClickListener(this::onClick);
         Button btnIntermediate = view.findViewById(R.id.button_intermediate);
         btnIntermediate.setOnClickListener(this::onClick);
+
+        CardView cardIntroduction = view.findViewById(R.id.card_introduction);
+        cardIntroduction.setOnClickListener(this::onClick);
     }
 
     @SuppressLint("NonConstantResourceId")
     public void onClick(View view) {
         switch (view.getId()){
+            case R.id.card_introduction:
             case R.id.button_notation:
-                Toast.makeText(getContext(),"Not Avaiable Yet", Toast.LENGTH_SHORT).show();
+                showPopup(view);
                 break;
             case R.id.button_beginner:
                 BeginnerFragment beginnerFragment = new BeginnerFragment();
@@ -57,5 +67,51 @@ public class MenuFragment extends Fragment {
                 Toast.makeText(getContext(),"Not Avaiable Yet", Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private void showPopup(View view) {
+//        View popupView = View.inflate(getContext(), R.layout.popup_action, null);
+
+        //Create a View object yourself through inflater
+        LayoutInflater inflater = (LayoutInflater) view.getContext().getSystemService(view.getContext().LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_action, null);
+
+        //Specify the length and width through constants
+        int width = LinearLayout.LayoutParams.MATCH_PARENT;
+        int height = LinearLayout.LayoutParams.MATCH_PARENT;
+
+        //Make Inactive Items Outside Of PopupWindow
+        boolean focusable = true;
+
+        //Create a window with our parameters
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        //Set the location of the window on the screen
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        //Set color etc
+        ImageView imageViewPop = popupView.findViewById(R.id.iv_popup_icon);
+        imageViewPop.setImageResource(R.drawable.monster2);
+        Button buttonPopStart = popupView.findViewById(R.id.button_mulai);
+        buttonPopStart.setBackgroundColor(R.color.blue);
+        buttonPopStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"Not Avaiable Yet", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Handler for clicking on the inactive zone of the window
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                //Close the window when clicked
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+
     }
 }
