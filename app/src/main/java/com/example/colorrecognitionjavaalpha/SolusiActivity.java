17b54964db947994a7e2236912e1eec5b7171aff
Zaki Burnama <zaki.burnama1017@gmail.com>
2023-10-06 07:14:14 +0700

    make solution activity

---
+++ app/src/main/java/com/example/colorrecognitionjavaalpha/SolusiActivity.java
@@ -0,0 +1,14 @@
+package com.example.colorrecognitionjavaalpha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SolusiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solusi);
+    }
+}
